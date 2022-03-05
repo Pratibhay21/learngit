9693f2eedabbc81075ffb7b7b390f82de170b217
Pratibha Yadav <yadavpratibha21111999@gmail.com>
2022-03-05 21:09:49 +0530

    adding array.c file

---
+++ array.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int arr[6]={89,78,67,77,90,55};
+    int i;
+    printf("array is: ");
+    for(i=0;i<=5;i++){
+        
+        printf("%d\t",arr[i]);
+    }
+    return 0;
+}
