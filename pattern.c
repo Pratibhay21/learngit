91169807433143f4de05b75cbd676e2ea642ab2a
Pratibha Yadav <yadavpratibha21111999@gmail.com>
2022-03-05 22:06:27 +0530

    added modification for lower triangle for pattern.c

--- pattern.c
+++ pattern.c
@@ -1,11 +1,20 @@
 #include<stdio.h>
 int main(){
     int i,j;
-    for(i=1;i<=5;i++){
+    for(i=1;i<=4;i++){ //modified
         for(j=1;j<i;j++){
             printf("*");
         }
         printf("\n");
     }
+
+    //modification
+    for(i=1;i<=5;i++){
+        for(j=i;j<5;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+
     return 0;
 }
