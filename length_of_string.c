985900fba3f44a2a04958096ed81f4d874958f23
Pratibha Yadav <yadavpratibha21111999@gmail.com>
2022-03-06 11:29:28 +0530

    added line splicing

--- length_of_string.c
+++ length_of_string.c
@@ -10,5 +10,6 @@ int main(){
         i++;
     }
     printf("\n College name is :%d",i);
+    printf("%s","\n you have to dream before your dream can come true.\ --A.P.J. Abdul Kalam"); //string splicing concept "\" "
     return 0;
 }
