82141552a5b7d4dbe237b5fb17e1ee3e593790f1
Pratibha Yadav <yadavpratibha21111999@gmail.com>
2022-03-08 20:21:15 +0530

    modified

--- first_last_char_of_string.c
+++ first_last_char_of_string.c
@@ -7,6 +7,6 @@ char *str;
 printf("Enter the string:");
 gets(str);
 int len = strlen(str); 
-printf("First: %c, Last: %c\n", str[0], str[len-1]);
+printf("First: %c, Last: %c\n", str[0], str[len-1]);  //as NULL('\0') is a part of string
     return 0;
 }
